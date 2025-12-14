3eb3ae0661f184658379f2841c545e5e3cbf6559
L1092006 <tanloc1092006@gmail.com>
2025-12-13 19:35:49 -0600

    add Booking and Menu model

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -73,11 +73,19 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#Settings.py 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            }
+        }
 }
 
 
--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 # Create your models here.
+
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True, validators=[MaxValueValidator(11)])
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(validators=[MaxValueValidator(6)])
+    booking_date = models.DateTimeField()
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True, validators=[MaxValueValidator(5)])
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
