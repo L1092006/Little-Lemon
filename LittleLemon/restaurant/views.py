282cb70d6e15833eb33566700a7f243e5e48c743
L1092006 <tanloc1092006@gmail.com>
2025-12-24 17:55:57 -0600

    add token auth from rest and add IsAuthenticated permission requirement for all views

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .models import *
 from .serializers import *
 
@@ -9,13 +10,16 @@ def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset  = Booking.objects.all()
     serializer_class = BookingSerializer
